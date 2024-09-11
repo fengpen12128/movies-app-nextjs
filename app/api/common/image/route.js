3eb0b3d3d9b9cfcf58177c6962267f9cd13b12f2
fengpan <fengp@epsoft.com.cn>
2024-09-11 16:31:42 +0800

    init

--- app/api/common/image/route.js
+++ app/api/common/image/route.js
@@ -1,7 +1,7 @@
-import { list } from "@vercel/blob";
+//import { list } from "@vercel/blob";
 
 export async function GET() {
-  const response = await list();
-  const allImages = response.blobs.map((blob) => blob.downloadUrl);
-  return Response.json({ wallpapers: allImages }, { status: 200 });
+  //   const response = await list();
+  //   const allImages = response.blobs.map((blob) => blob.downloadUrl);
+  return Response.json({}, { status: 200 });
 }
