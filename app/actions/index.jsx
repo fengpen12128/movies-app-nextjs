41c32299e06437641ea5c61e5335d383bbb484c0
fengpan <fengp@epsoft.com.cn>
2024-09-19 22:53:36 +0800

    重构+更好的适合服务端渲染

--- app/actions/index.jsx
+++ app/actions/index.jsx
@@ -1,7 +1,91 @@
-'use server'
+"use server";
 
 import prisma from "@/app/lib/prisma";
 import { revalidatePath } from "next/cache";
+import dayjs from "dayjs";
+
+export async function getDownloadMovies(page = 1, collected) {
+  try {
+    const pageSize = 50;
+    const skip = (page - 1) * pageSize;
+
+    const collectionMovies = await prisma.moviesCollection.findMany({
+      select: {
+        movieCode: true,
+      },
+    });
+    const collectionMovieCode = collectionMovies.map((x) => x.movieCode);
+
+    let q = {
+      where: {
+        ...(collected === "true" && {
+          movieCode: {
+            in: collectionMovieCode,
+          },
+        }),
+        ...(collected === "false" && {
+          movieCode: {
+            notIn: collectionMovieCode,
+          },
+        }),
+      },
+      include: {
+        MovieInfo: {
+          include: {
+            files: {
+              where: {
+                type: 2,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        createdTime: "desc",
+      },
+      skip,
+      distinct: ["movieCode"], // Add this line to ensure unique movieCode entries
+
+      take: pageSize,
+    };
+
+    let [downloadMoviesResult, totalCount] = await Promise.all([
+      prisma.moviesVideoResource.findMany(q),
+      prisma.moviesVideoResource.count({ where: q.where }),
+    ]);
+
+    const downloadMovies = downloadMoviesResult.map((x) => ({
+      downloadTime: x.createdTime,
+      ...x.MovieInfo,
+    }));
+
+    downloadMovies.forEach((x) => {
+      try {
+        x.releaseDate = dayjs(x.releaseDate || "2000-01-01").format(
+          "YYYY-MM-DD"
+        );
+        x.collected = collectionMovieCode.includes(x.code);
+        x.downloaded = true;
+        x.coverUrl = x.files[0]?.path;
+        delete x.files;
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    return {
+      pagination: {
+        totalCount,
+        current: page,
+        pageSize,
+        totalPage: Math.ceil(totalCount / pageSize),
+      },
+      movies: downloadMovies,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export async function toggleCollection(code, refreshPath) {
   try {
