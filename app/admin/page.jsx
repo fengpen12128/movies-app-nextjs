2311457e6bc6e7d0d75a58de47bf3976fb04cd6e
fengpan <fengp@epsoft.com.cn>
2024-10-09 01:01:25 +0800

    Update page.jsx

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -69,9 +69,6 @@ import {
 } from "@/components/ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-export const description =
-  "An orders dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. The main area has a list of recent orders with a filter and export button. The main area also has a detailed view of a single order with order details, shipping information, billing information, customer information, and payment information.";
-
 const Page = () => {
   return (
     <>
