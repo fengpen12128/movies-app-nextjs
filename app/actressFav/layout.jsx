1d4d5404fa33402658d538371b8b6513647c9de6
fengpan <fengp@epsoft.com.cn>
2024-10-10 19:18:12 +0800

    Update layout.jsx

--- app/actressFav/layout.jsx
+++ app/actressFav/layout.jsx
@@ -1,4 +1,6 @@
 import SharedLayout from "../shared-layout/layout";
+import { Suspense } from "react";
+import ScreenLoading from "@/components/ScreenLoading";
 
 export const metadata = {
   title: "收藏演员",
